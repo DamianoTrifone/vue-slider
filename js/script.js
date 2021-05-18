00d07a5375f2c50382e570e595e948c3b31e0ee9
DamianoTrifone <trifone.damiano@gmail.com>
2021-05-18 16:08:32 +0200

    Collegamento e posizionamento foto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            images: [
+                "https://images.everyeye.it/img-notizie/40-ore-video-paesaggi-mozzafiato-pianeta-v3-299389.jpg",
+                "https://www.ilovevaldinon.it/wp-content/uploads/2020/02/alpe-di-siusi-1.jpg.webp",
+                "https://miro.medium.com/max/1200/1*Gsq1yHsb3cV2JKuj5c_SoQ.jpeg",
+                "https://www.zingarate.com/pictures/2020/12/15/odle.jpeg",
+                "https://www.greenme.it/wp-content/uploads/2015/11/paesaggi-.jpg"
+            ],
+        }
+    }
+);
